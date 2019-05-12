173caec9dc4d0a2199763a003ff1fbf582f0f46d
mlgmxyysd <1204074991@qq.com>
2019-05-12 23:20:19 +0800

    0.4.1.2

---
+++ README.md
@@ -0,0 +1,7 @@
+# EdXposed SandHook Add-on
+
+**An add-on for enable SandHook features in EdXposed.**
+
+- You need to install Riru-EdXposed(YAHFA) first.
+
+- Riru-EdXposed(YAHFA) + SandHook Add-on = Riru-EdXposed(SandHook)
---
+++ install.sh
@@ -0,0 +1,18 @@
+SKIPMOUNT=false
+PROPFILE=false
+POSTFSDATA=false
+LATESTARTSERVICE=false
+REPLACE="
+"
+print_modname() {
+  ui_print "*******************************"
+  ui_print "*   EdXposed SandHook Add-on  *"
+  ui_print "*******************************"
+}
+on_install() {
+  ui_print "- Extracting module files"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+set_permissions() {
+  set_perm_recursive $MODPATH 0 0 0755 0644
+}
